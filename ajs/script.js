dbdaa11f4cf86c4dae574793a460cd67b5bd5f16
Mr. Thristan Ezra <thristanezraa32@gmail.com>
2025-05-21 07:58:31 +0700

    Add files via upload

---
+++ ajs/script.js
@@ -0,0 +1,195 @@
+
+// -- Question 1
+function q1b1function() {
+	document.getElementById("q1b1").innerText = "✅ Correct. You did it!"
+}
+
+function q1b2function() {
+	document.getElementById("q1b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q1b3function() {
+	document.getElementById("q1b3").innerText = "❌ Incorrect. Please Try Again."
+}
+
+// -- Question 2
+function q2b1function() {
+	document.getElementById("q2b1").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q2b2function() {
+	document.getElementById("q2b2").innerText = "✅ Correct. You did it!"
+}
+
+function q2b3function() {
+	document.getElementById("q2b3").innerText = "❌ Incorrect. Please Try Again."
+}
+
+// -- Question 3
+function q3b1function() {
+	document.getElementById("q3b1").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q3b2function() {
+	document.getElementById("q3b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q3b3function() {
+	document.getElementById("q3b3").innerText = "✅ Correct. You did it!"
+}
+
+// -- Question 4
+function q4b1function() {
+	document.getElementById("q4b1").innerText = "✅ Correct. You did it!"
+}
+
+function q4b2function() {
+	document.getElementById("q4b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q4b3function() {
+	document.getElementById("q4b3").innerText = "❌ Incorrect. Please Try Again."
+}
+
+// -- Question 5
+function q5b1function() {
+	document.getElementById("q5b1").innerText = "✅ Correct. You did it!"
+}
+
+function q5b2function() {
+	document.getElementById("q5b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q5b3function() {
+	document.getElementById("q5b3").innerText = "❌ Incorrect. Please Try Again."
+}
+
+// -- Question 6
+function q6b1function() {
+	document.getElementById("q6b1").innerText = "✅ Correct. You did it!"
+}
+
+function q6b2function() {
+	document.getElementById("q6b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q6b3function() {
+	document.getElementById("q6b3").innerText = "❌ Incorrect. Please Try Again."
+}
+
+// -- Question 7
+function q7b1function() {
+	document.getElementById("q7b1").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q7b2function() {
+	document.getElementById("q7b2").innerText = "✅ Correct. You did it!"
+}
+
+function q7b3function() {
+	document.getElementById("q7b3").innerText = "❌ Incorrect. Please Try Again."
+}
+
+// -- Question 8
+function q8b1function() {
+	document.getElementById("q8b1").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q8b2function() {
+	document.getElementById("q8b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q8b3function() {
+	document.getElementById("q8b3").innerText = "✅ Correct. You did it!"
+}
+
+// -- Question 9
+function q9b1function() {
+	document.getElementById("q9b1").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q9b2function() {
+	document.getElementById("q9b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q9b3function() {
+	document.getElementById("q9b3").innerText = "✅ Correct. You did it!"
+}
+
+// -- Question 10
+function q10b1function() {
+	document.getElementById("q10b1").innerText = "✅ Correct. You did it!"
+}
+
+function q10b2function() {
+	document.getElementById("q10b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q10b3function() {
+	document.getElementById("q10b3").innerText = "❌ Incorrect. Please Try Again."
+}
+
+// -- Question 11
+function q11b1function() {
+	document.getElementById("q11b1").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q11b2function() {
+	document.getElementById("q11b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q11b3function() {
+	document.getElementById("q11b3").innerText = "✅ Correct. You did it!"
+}
+
+// -- Question 12
+function q12b1function() {
+	document.getElementById("q12b1").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q12b2function() {
+	document.getElementById("q12b2").innerText = "✅ Correct. You did it!"
+}
+
+function q12b3function() {
+	document.getElementById("q12b3").innerText = "❌ Incorrect. Please Try Again."
+}
+
+// -- Question 13
+function q13b1function() {
+	document.getElementById("q13b1").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q13b2function() {
+	document.getElementById("q13b2").innerText = "✅ Correct. You did it!"
+}
+
+function q13b3function() {
+	document.getElementById("q13b3").innerText = "❌ Incorrect. Please Try Again."
+}
+
+// -- Question 14
+function q14b1function() {
+	document.getElementById("q14b1").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q14b2function() {
+	document.getElementById("q14b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q14b3function() {
+	document.getElementById("q14b3").innerText = "✅ Correct. You did it!"
+}
+
+// -- Question 15
+function q15b1function() {
+	document.getElementById("q15b1").innerText = "✅ Correct. You did it!"
+}
+
+function q15b2function() {
+	document.getElementById("q15b2").innerText = "❌ Incorrect. Please Try Again."
+}
+
+function q15b3function() {
+	document.getElementById("q15b3").innerText = "❌ Incorrect. Please Try Again."
+}
